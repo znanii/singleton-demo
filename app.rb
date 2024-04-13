e8dfe334e05be8fe47e763e571aa9fd7a93ac267
zhanii <naborznanii@gmail.com>
2024-04-12 20:26:25 -0700

    @f initialize

--- app.rb
+++ app.rb
@@ -1,14 +1,18 @@
 class Logger
+  def initialize
+    @f = File.open 'log.txt', 'a'
+  end
+
   def self.say_something
     puts 'haha'
   end
 
-  def self.log_smth what
-    f = File.open 'log.txt', 'a'
-    f.puts what
-    f.close
+  def log_smth what
+    @f.puts what
   end
 end
 
 Logger.say_something
-Logger.log_smth 'wazzup'
+
+logger = Logger.new
+logger.log_smth 'HEYYY'
