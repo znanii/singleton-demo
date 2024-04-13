157eaf831843387f65237ff472e2cf880c061861
zhanii <naborznanii@gmail.com>
2024-04-12 20:37:51 -0700

    update logger

--- app.rb
+++ app.rb
@@ -3,16 +3,31 @@ class Logger
     @f = File.open 'log.txt', 'a'
   end
 
+@@x= nil
+
+def self.instance #создание экземпляра класса
+   if @@x == nil
+    @@x = Logger.new
+  end
+
+  return @@x
+end
+
+#class method
   def self.say_something
     puts 'haha'
   end
 
+#instance method
   def log_smth what
     @f.puts what
   end
 end
 
 Logger.say_something
+Logger.instance.log_smth 'blabla' 
+Logger.instance.log_smth 'blabla22' 
 
 logger = Logger.new
 logger.log_smth 'HEYYY'
+
