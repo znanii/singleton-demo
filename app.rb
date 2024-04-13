b8bfa9c3069aae4216fab20dcab015527961f994
zhanii <naborznanii@gmail.com>
2024-04-12 20:20:23 -0700

    add log_smth

--- app.rb
+++ app.rb
@@ -2,6 +2,13 @@ class Logger
   def self.say_something
     puts 'haha'
   end
+
+  def self.log_smth what
+    f = File.open 'log.txt', 'a'
+    f.puts what
+    f.close
+  end
 end
 
 Logger.say_something
+Logger.log_smth 'wazzup'
